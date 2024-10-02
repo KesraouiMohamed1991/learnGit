d31f081175646aeba59e2844858add07dd853a6c
Kesraoui Mohamed <92538732+KesraouiMohamed1991@users.noreply.github.com>
2024-10-02 12:08:39 +0200

    adding code

---
+++ index.js
@@ -0,0 +1,6 @@
+const root = document.getElementById("root");
+const h1 = document.createElement("h1");
+h1.innerHTML = "Hello World";
+root.appendChild(h1);
+h1.style.color = "red";
+h1.style.fontSize = "50px";
